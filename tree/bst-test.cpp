7120a7c895311bb3f958b1963b4ea07dfd759055
mohit-baliyan <mohitbaliyan98@gmail.com>
2021-10-10 03:13:50 +0100

    Binary Search Tree Oct 10

--- tree/bst-test.cpp
+++ tree/bst-test.cpp
@@ -8,10 +8,10 @@ TEST(MyTestSuitName, MyTestCaseName) {
 
     BST obj;
 
-    Node * root = obj.add(10,NULL);
-    root = obj.add(5,root);
-    root = obj.add(15,root);
-    root = obj.add(20,root);
+    Node *root = obj.add(10, NULL);
+    root = obj.add(5, root);
+    root = obj.add(15, root);
+    root = obj.add(20, root);
     root = obj.add(8, root);
 
     obj.inorder(root);
@@ -20,11 +20,10 @@ TEST(MyTestSuitName, MyTestCaseName) {
     cout << endl;
     obj.postorder(root);
 
+    ASSERT_EQ(3, obj.height(root)) << "Height of above bst should be equal to 4";
 
-    ASSERT_EQ( 3, obj.height(root) ) << "Height of above bst should be equal to 4";
+    ASSERT_EQ(20, obj.maximum(root)) << "Maximum element in above bst should be equal to 20";
 
-    ASSERT_EQ( 20, obj.maximum(root) ) << "Maximum element in above bst should be equal to 20";
-
-    ASSERT_EQ( 5, obj.minimum(root) ) << "Minimum element in above bst should be equal to 5";
+    ASSERT_EQ(5, obj.minimum(root)) << "Minimum element in above bst should be equal to 5";
 
 }
